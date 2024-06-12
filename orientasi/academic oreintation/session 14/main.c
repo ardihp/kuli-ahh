708a4406d2f232687312c3f273e8a83445b53a85
ardihp <ardihp8@gmail.com>
2024-06-12 23:28:38 +0700

    feat: orientasi academic

---
+++ orientasi/academic oreintation/session 14/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() // <-- "function main"
+{
+    int umur;  // <-- "variable"
+    umur = 10; // <-- "peran dari ="
+
+    printf("Isi dari variable umur adalah %d \n", umur);
+
+    return (0);
+}
