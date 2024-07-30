c21bde15192d830fd5398380d4df41210afea78b
ardihp <ardihp8@gmail.com>
2024-07-30 17:13:03 +0700

    fix: algo form session 15 bubble

--- algo programming/form/session-15/bubble-sort.c
+++ algo programming/form/session-15/bubble-sort.c
@@ -8,12 +8,12 @@ void bubbleSort(int list_number[], int list_size)
     {
         for (j = 0; j < list_size; j++)
         {
-            printf("j=%d   i=%d \n", list_number[j], list_number[i]);
+            temp = list_number[j];
 
             if (list_number[j] > list_number[i])
             {
                 list_number[j] = list_number[i];
-                list_number[i] = list_number[j];
+                list_number[i] = temp;
             }
         }
     }
@@ -28,8 +28,13 @@ void bubbleSort(int list_number[], int list_size)
 
 int main()
 {
-    int list_number[] = {3, 2, 1};
+    int list_number[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
     int list_size = sizeof(list_number) / sizeof(list_number[0]);
+
+    for (int i = 0; i < list_size; i++)
+    {
+        printf("%d ", list_number[i]);
+    }
 
     bubbleSort(list_number, list_size);
 
